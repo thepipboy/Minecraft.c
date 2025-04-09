6300c76638706f2c69806a9f3c7ca8ab756530f2
thepipboy <3476647793@qq.com>
2025-04-09 14:46:06 +0800

    Update MineCraft.c

--- MineCraft.c
+++ MineCraft.c
@@ -4,6 +4,7 @@ short XYZ(small,middle,big,n){
     int small = 3;
     int middle = 4;
     int big = 5;
+void X(){
     small.X = for{{n,0,n},
                   {0,n,0},
                   {n,0,n}}
@@ -21,16 +22,17 @@ short XYZ(small,middle,big,n){
                 {0,n,0,n,0},
                 {n,0,0,0,n}}
     return [1,2,3,4,5,6,7,8,9]
-    
+          }
+void Y(){
     small.Y = for{{n, 0, n},
                   {0, n, 0},
-                  {0, n, n}}
+                  {0, n, 0}}
     return [1,2,3,4,5,6,7,8,9]
     
     middle.Y = for{{n,0,0,n},
                    {0,n,n,0},
                    {0,n,n,0},
-                   {n,0,0,n}}
+                   {0,n,n,0}}
     return [1,2,3,4,5,6,7,8,9]
 
     big.Y = for{{n,0,0,0,n},
@@ -39,7 +41,8 @@ short XYZ(small,middle,big,n){
                 {0,0,n,0,0},
                 {0,0,n,0,0}}
     return [1,2,3,4,5,6,7,8,9]
-
+        }
+void Z(){
     small.Z = for{{n, n, n},
                   {0, n, 0},
                   {n, n, n}}
@@ -50,15 +53,75 @@ short XYZ(small,middle,big,n){
                    {0,n,0,0},
                    {n,n,n,n}}
     return [1,2,3,4,5,6,7,8,9]
-
+   
     big.Z = for{{n,n,n,n,n},
                 {0,0,0,n,0},
                 {0,0,n,0,0},
                 {0,n,0,0,0},
                 {n,n,n,n,n}}
     return [1,2,3,4,5,6,7,8,9]    
-}
+ 
+    class N(){ 
+    small.N(n){
+        for SmallZ in range([n, 0, n],
+                            [n, n, n],
+                            [n, 0, n]){
+            return [1,2,3,4,5,6,7,8,9]
+          
+    Middle.N(n){
+        for MiddleZ in range([n, 0, 0, n],
+                             [n, n, 0, n],
+                             [n, 0, n, n],
+                             [n, 0, 0, n]){
+            return [1,2,3,4,5,6,7,8,9]
+        
+    Big.N(n){
+        for BigZ in range([n, 0, 0, 0, n],
+                          [n, n, 0, 0, n],
+                          [n, 0, n, 0, n],
+                          [n, 0, 0, n, n],
+                          [n, 0, 0, 0, n]){
+            return [1,2,3,4,5,6,7,8,9]
+            }
+void three(n){
+        for inner in range([n-4],[n-3],[n-2],
+                           [n-1],[n] ,[n+1],
+                           [n+2],[n+3],[n+4]){
+            return [1,2,3,4,5,6,7,8,9]
+        
+        for outer in range([n+4],[n+3],[n+2],
+                           [n+1],[n] ,[n-1],
+                           [n-2],[n-3],[n-4]){
+            return [9,8,7,6,5,4,3,2,1]
+            }
+void four(n){
+        for inner in range([n+1],[n+1],[n+1],[n+1],
+                           [n+1],[n],[n],[n+1],
+                           [n+1],[n],[n],[n+1],
+                           [n+1],[n+1],[n+1],[n+1]){
+            return [0,1,2,3,4,5,6,7,8,9]
+        
+        for outer in range([n],[n],[n],[n],
+                           [n],[n-1],[n-1],[n],
+                           [n],[n-1],[n-1],[n],
+                           [n],[n],[n],[n]){
+            return [1,2,3,4,5,6,7,8,9,0]
+            }
+void five(n){}
+        for innerL in range([n],[n+1],[n+2],[n+3],[n+4],
+                           [n+15],[n+16],[n+17],[n+18],[n+5],
+                           [n+14],[n+23],[n+24],[n+19],[n+6],
+                           [n+13],[n+22],[n+21],[n+20],[n+7],
+                           [n+12],[n+11],[n+10],[n+9],[n+8]){
+            return  [1,-3,-8,-14]
 
+        for innerR in range([n+1],[n+16],[n+15],[n+14],[n+13],
+                           [n+2],[n+17],[n+24],[n+23],[n+12],
+                           [n+3],[n+18],[n+25],[n+22],[n+11],
+                           [n+4],[n+19],[n+20],[n+21],[n+10],
+                           [n+5],[n+6],[n+7],[n+8],[n+9]){
+            return [0,-4,-9,-15] 
+            }
 int vertices(x, y, z, n){
     return [
         x-n,y+n,z-n, x-n,y+n,z+n, x+n,y+n,z+n, x+n,y+n,z-n; #a
@@ -73,7 +136,7 @@ typedef struct {
 float sphere(x , y , z , r){
     return [r^2 === x^2 + y^2 + z^2]
         }
-double +coordinate(x, y, z , N):
+double +coordinate(x, y, z , N){
     N = 1 / n
     dx = x * N
     dy = y * N
@@ -126,4 +189,5 @@ unsinged Volume(){
      a * tan(x + t10) + b * atan(x - t10) + c * tan(y + t11) + d * atan(y - t11) + e * tan(z + t12) + f * atan(z - t12)    #t101112
          ]
 return [vertice - edge + face = 2]
+return [three = 3, four = 4 , five = 5]
 }
